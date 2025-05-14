8d2cb95e877237779e5a36c9454d8b4b6b885e54
Daniel Schoenig <daniel.schoenig@protonmail.com>
2025-05-14 15:11:55 -0400

    Update workshop CEF 2025

--- lab/script_atelier_validation.R
+++ lab/script_atelier_validation.R
@@ -5,25 +5,26 @@
 # Avant d'installer les paquets, il faut s'assurer de disposer de la
 # version la plus récente de R (https://cran.r-project.org/)
 
-install.packages(c("DHARMa",
-                   "data.table",
-                   "lme4",
-                   "Matrix",
-                   "TMB",
-                   "glmmTMB",
-                   "mgcv",
-                   "mgcViz",
-                   "gratia",
-                   "marginaleffects",
-                   "ggplot2",
-                   "colorspace",
-                   "sf"))
+install.packages(c(
+  "DHARMa",
+  "data.table",
+  "lme4",
+  "Matrix",
+  "TMB",
+  "glmmTMB",
+  "mgcv",
+  "mgcViz",
+  "gratia",
+  "marginaleffects",
+  "ggplot2",
+  "colorspace",
+  "sf"
+))
 
 
 #####################################################################
 # 1. CONFIGURATION ##################################################
 #####################################################################
-
 
 # Charger les paquets
 
@@ -44,31 +45,37 @@ options(show.signif.stars = FALSE)
 base.size <- 11
 plot_theme <-
   theme_light(base_size = base.size) +
-  theme(plot.title = element_text(hjust = 0,
-                                  face = "bold",
-                                  margin = margin(l = 0, b = base.size/3, t = base.size/3)),
-        plot.tag = element_text(face = "bold"),
-        axis.line.x = element_line(color = "black",
-                                   linewidth = rel(0.5)),
-        axis.line.y = element_line(color = "black",
-                                   linewidth = rel(0.5)),
-        axis.title.x = element_text(margin = margin(t = base.size/2)),
-        axis.title.y = element_text(margin = margin(r = base.size/2)),
-        legend.position = "right",
-        legend.justification = "top",
-        legend.key.size = unit(base.size, "pt"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        plot.margin = margin(3, 3, 3, 3),
-        strip.text = element_text(size = rel(0.8),
-                                  hjust = 0.5,
-                                  color = "black",
-                                  margin = margin(base.size/2,
-                                                  base.size/2,
-                                                  base.size/2,
-                                                  base.size/2)),
-        strip.background = element_rect(fill = "gray90", colour = NA))
+  theme(
+    plot.title = element_text(
+      hjust = 0,
+      face = "bold",
+      margin = margin(l = 0, b = base.size / 3, t = base.size / 3)
+    ),
+    plot.tag = element_text(face = "bold"),
+    axis.line.x = element_line(color = "black", linewidth = rel(0.5)),
+    axis.line.y = element_line(color = "black", linewidth = rel(0.5)),
+    axis.title.x = element_text(margin = margin(t = base.size / 2)),
+    axis.title.y = element_text(margin = margin(r = base.size / 2)),
+    legend.position = "right",
+    legend.justification = "top",
+    legend.key.size = unit(base.size, "pt"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    plot.margin = margin(3, 3, 3, 3),
+    strip.text = element_text(
+      size = rel(0.8),
+      hjust = 0.5,
+      color = "black",
+      margin = margin(
+        base.size / 2,
+        base.size / 2,
+        base.size / 2,
+        base.size / 2
+      )
+    ),
+    strip.background = element_rect(fill = "gray90", colour = NA)
+  )
 theme_set(plot_theme)
 
 
@@ -79,11 +86,9 @@ theme_set(plot_theme)
 setwd("DOSSIER")
 
 
-
 #####################################################################
 # Exemple 1: Croissance de'l Épinette de Norvège dans les Alpes #####
 #####################################################################
-
 
 # Importer les données
 
@@ -98,19 +103,14 @@ gutten
 # Graphique exploratoire
 
 ggplot(gutten) +
-  geom_point(aes(x = age.base, y = volume, colour = quality),
-             alpha = 0.5) +
+  geom_point(aes(x = age.base, y = volume, colour = quality), alpha = 0.5) +
   facet_wrap(vars(quality)) +
-  labs(y = "Volume (m³/1000)",
-       x = "Age (years)",
-       colour = "Site quality")
-
+  labs(y = "Volume (m³/1000)", x = "Age (years)", colour = "Site quality")
 
 
 #####################################################################
 # Exemple 2: Effet de l'ozone sur les semis de l'épinette de Sitka ##
 #####################################################################
-
 
 # Importer les données
 
@@ -125,21 +125,18 @@ sitka
 # Graphique exploratoire
 
 ggplot(sitka) +
-  geom_line(data = sitka,
-            aes(x = day, y = size,
-                group = tree.id, colour = treatment),
-            alpha = 0.5) +
+  geom_line(
+    data = sitka,
+    aes(x = day, y = size, group = tree.id, colour = treatment),
+    alpha = 0.5
+  ) +
   scale_colour_brewer(type = "qual", palette = "Set1") +
-  labs(x = "Time (days)",
-       y = "Size (cm²m)",
-       colour = "Treatment")
-
+  labs(x = "Time (days)", y = "Size (cm²m)", colour = "Treatment")
 
 
 #####################################################################
 # Exemple 3: Distribution des lichens en Suède ######################
 #####################################################################
-
 
 # Importer les données
 
@@ -154,7 +151,7 @@ lichen
 
 # Modifier le thème pour ggplot2
 
-plot_theme <- 
+plot_theme <-
   plot_theme +
   theme(panel.grid.major = element_line(linewidth = rel(1)))
 theme_set(plot_theme)
@@ -164,8 +161,9 @@ theme_set(plot_theme)
 
 lichen.prop.agg <-
   lichen[,
-         .(occurrence = mean(occurrence)),
-         by = .(ip, species, east.agg, north.agg)]
+    .(occurrence = mean(occurrence)),
+    by = .(ip, species, east.agg, north.agg)
+  ]
 
 ggplot(lichen.prop.agg) +
   geom_raster(aes(x = east.agg, y = north.agg, fill = occurrence)) +
